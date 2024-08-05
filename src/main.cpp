8055ee059610dd7ea0ac4e00562ed56d248db510
JovinRyan <jovinryanj@tamu.edu>
2024-08-05 20:50:09 +0530

    Added CLI11 dependency.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
+#include <fstream>
+
+#include "cli/CLI11.hpp"
+
 #include "containers/dump_data_container.h"
 #include "read_write/read_file.h"
+#include "calculations/ddc_get_displacement.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-  if (argc < 2)
-  {
-    std::cerr << "No LAMMPS Dump File Provided!\n";
+  CLI::App olt{"OpenLAMMPSToolbox App"};
 
-    return 1;
-  }
+  // Defining options
+  std::ifstream infile;
+  std::string ftype = "";
+  std::string function = "";
+  double disp_threshold = 1;
 
-  std::cout << "Reading Custom Dump File: " << argv[1] << "\n";
-  std::ifstream infile(argv[1]);
-  if (infile.is_open() && infile.good())
+  int p = 0;
+  olt.add_option("--f", infile, "Required LAMMPS Dump File")
+      ->required()
+      ->check(CLI::ExistingFile);
+
+  olt.add_option("--ftype", ftype, "Dump File Type");
+
+  olt.add_option("--function", function, "Post-Processing Function");
+
+  olt.add_option("--disp_threshold", disp_threshold, "Lattice Parameter");
+
+  CLI11_PARSE(olt, argc, argv);
+
+  if (ftype == "xyz" & function == "displacement")
   {
-    dump_data_container test = customToDumpData(infile);
+    dump_data_container xyz_ddc = xyzToDumpData(infile);
+
+    std::vector<int> disp_vec = get_displacement_vec(xyz_ddc, disp_threshold);
   }
 
   return 0;
