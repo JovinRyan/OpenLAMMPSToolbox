0f58a4e01e0fc8017f441a91cadc5712382e72ac
JovinRyan <jovinryanj@tamu.edu>
2024-08-07 21:14:10 +0530

    Added function to check whether a vector of atoms is sorted.

---
+++ include/utils/sort_checker.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <vector>
+#include <random>
+#include <iostream>
+
+#include "containers/atom_classes.h"
+
+bool id_sort_check(std::vector<atom> &in_atom_vec);
---
+++ src/utils/sort_checker.cpp
@@ -0,0 +1,19 @@
+#include "utils/sort_checker.h"
+
+bool id_sort_check(std::vector<atom> &in_atom_vec)
+{
+  std::random_device rd;                                        // Only used once to initialise (seed) engine
+  std::mt19937 rng(rd());                                       // Random-number engine used (Mersenne-Twister in this case)
+  std::uniform_int_distribution<int> uni(1, size(in_atom_vec)); // Guaranteed unbiased
+
+  for (int i = 0; i < int(size(in_atom_vec) * 0.05); i++)
+  {
+    int random_int = uni(rng);
+    if (int(in_atom_vec[random_int].get_id()) != random_int + 1)
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
