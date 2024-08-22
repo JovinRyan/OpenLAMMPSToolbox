162b82659715ca2a66303147c4f3cac75f4661f9
JovinRyan <jovinryanj@tamu.edu>
2024-08-22 08:17:12 -0500

    Added sort checking testing.

---
+++ include/tests/sort_check_test.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vector>
+#include <random>
+#include <memory>
+
+#include "containers/atom_classes.h"
+#include "utils/atom_creator.h"
+#include "utils/sort_checker.h"
+
+std::pair<bool, bool> id_sort_check_test();
--- src/main_tests.cpp
+++ src/main_tests.cpp
@@ -3,6 +3,7 @@
 #include "tests/doctest.h"
 #include "tests/atom_quicksort_test.h"
 #include "tests/atom_method_test.h"
+#include "tests/sort_check_test.h"
 
 TEST_CASE("Atom QuickSort Test")
 {
@@ -12,4 +13,11 @@ TEST_CASE("Atom QuickSort Test")
 TEST_CASE("Atom Get Distance Test")
 {
   CHECK(atom_dist_test());
+}
+
+TEST_CASE("Atom ID Sort Check Test")
+{
+  CHECK(id_sort_check_test().first);
+
+  CHECK(!(id_sort_check_test().second));
 }
